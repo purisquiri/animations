ab89caf6a5dc039b53b5ecff11d84a6947c31049
purisquiri <mau.dorigo@outlook.es>
2021-06-14 16:20:33 -0400

    zoom animations

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <button type="button" @click="flag = !flag">Toggle</button>
-  <transition name="fade" mode="out-in">
+  <!-- <transition name="fade" mode="out-in">
     <h2 v-if="flag" key="main">Hello Wolrd</h2>
     <h2 v-else key="secondary">Another Hello W</h2>
+  </transition> -->
+  <transition name="zoom">
+    <h2 v-if="flag">Hello</h2>
   </transition>
 </template>
 
@@ -18,16 +21,44 @@ export default {
 </script>
 
 <style>
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
 .fade-enter-from {
   opacity: 0;
 }
 
 .fade-enter-active {
-  transition: all 3s linear;
+  transition: all 1s linear;
 }
 
 .fade-leave-to {
-  transition: all 3s linear;
+  transition: all 1s linear;
   opacity: 0;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+}
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+}
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
